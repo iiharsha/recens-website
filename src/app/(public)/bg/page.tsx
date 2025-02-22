da23c57d6605f1ffaad654546029650e692c0365
Harsha <harshavadluri.dev@gmail.com>
2025-02-22 23:58:04 +0530

    meta pixel code

---
+++ src/app/(public)/bg/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx (Next.js App Router)
+export default function Home() {
+  return (
+    <div className="relative h-screen w-screen bg-white flex items-center justify-center">
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_20%_20%_at_center,_rgba(0,0,0,1)_0%,_rgba(255,255,255,1)_70%)]"></div>
+    </div>
+  );
+}
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ const Footer = dynamic(() => import("@/components/sections/Footer"), {
 // styles
 import "@/styles/globals.css";
 import { montserrat } from "@/fonts/fonts";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -79,16 +80,44 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-PWQRW7QQ" />
-      <GoogleAnalytics gaId="G-8XGNZRV5GF" />
-      <Head>
-        <link
-          rel="canonical"
-          href="https://recens.co.in"
-          key="canonical"
-        ></link>
-      </Head>
+      <head>
+        <link rel="canonical" href="https://recens.co.in" key="canonical" />
+      </head>
       <body className={`${montserrat.className} bg-[#FFFFFF]`}>
+        {/* Google Tag Manager & Analytics */}
+        <GoogleTagManager gtmId="GTM-PWQRW7QQ" />
+        <GoogleAnalytics gaId="G-8XGNZRV5GF" />
+
+        {/* Meta Pixel Code */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '540342605090965');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=540342605090965&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <Suspense fallback={<Loading />}>
           <Header />
           <main>
@@ -98,6 +127,5 @@ export default async function RootLayout({
           <Footer />
         </Suspense>
       </body>
-    </html>
-  );
+    </html>);
 }
